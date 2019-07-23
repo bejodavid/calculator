3f472cf95fa5917b172251ab30e335cd2d837ac5
guszti <agostonbejo1@gmail.com>
2019-07-23 23:23:42 +0200

    first real commit

---
+++ src/david/practice/DavidsProgram.java
@@ -0,0 +1,29 @@
+package david.practice;
+
+import java.util.Scanner;
+
+class DavidsProgram{
+	
+	public static void main(String[] args) {
+System.out.println("Hello Anyu ezt a játékot neked programoztam az a játékot neked terveztem és programoztam.");
+System.out.println("Az a játék lényege hogy  válaszolj rá.");
+System.out.println("Na itt is van az első kérdés:mennyire örülsz ennek a játéknak?");
+System.out.println("1=nagyon 2=egy picit 3=so-so 4=nem örülök ");
+		Scanner A = new Scanner(System.in);
+		int a;
+		a= A.nextInt();
+if(a==1) {System.out.println("Ügyes vagy, mert erre nem volt helyes válasz");
+}
+else {System.out.println("Ügyes vagy,mert erre nem volt helyes válasz");}	
+	
+
+System.out.println("Na Anyu,itt van a második kérdés:mennyi volt az IQ-m");
+System.out.println("1=30 2=112 3=148");
+int b;
+Scanner B = new Scanner(System.in);
+b= B.nextInt();
+if(b==2);{System.out.println("Ügyes vagy,tényleg 111 volt az IQ-m");}
+
+else{System.out.println();}
+}
+}
