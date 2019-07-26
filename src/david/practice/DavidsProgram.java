58d10a06776975fbba54a9c94068ead3dc7aa8bc
guszti <agostonbejo1@gmail.com>
2019-07-26 16:04:55 +0200

    Sorry the previous version was still experimental but this should be good

--- src/david/practice/DavidsProgram.java
+++ src/david/practice/DavidsProgram.java
@@ -13,16 +13,25 @@ System.out.print("press 1 to go to the calculator press 2 to see the realise sce
 int A;
 if(A==2) {System.out.println("Welcome to the realise scedule.  ");
 System.out.println("0.0.0.0.1, 2019.july.26");
-System.out.println("0.0.0.0.2 2019.july 28");
-System.out.println("0.0.0.0.3 2019.july 31");};
-if(A==1){System.out.println("Welcome to the calculator. For now you can only + or - but if you want to know when you can calculate more complex eqasions chek the realise schedule");
-  System.out.println("Press + or - depending on wich eqaison you want to do");
+System.out.println("0.0.0.0.2, 2019.july.28");
+System.out.println("0.0.0.0.3, 2019.july.31");};
+if(A==1){System.out.println("Welcome to the calculator. For now you can only + and - but if you want to know when you can calculate more complex eqasions chek the realise schedule");
+  System.out.println("Press +,-,*,/ depending on wich eqaison you want to do");
+ String B = new BufferedReader(new InputStreamReader(System.in)).readLine();
    if(B.equals("-")) {System.out.println("Now pick two numbers and then the computer will do the rest. ");
    int E;
    int F;
-   System.out.println(E-F);};
+   System.out.println(E-F);
+   };
+   if(B.equals("+")) {System.out.println("Now pick two numbers and then the computer will do the rest. ");
+   int I;
+   int G;
+   System.out.println(I+G);};
+   
+   };
    
   }
 }
-}
+
+
 	
