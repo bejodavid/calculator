e1d5db033d256ec151fd919fb069bc8e3fc94b44
guszti <agostonbejo1@gmail.com>
2019-07-27 09:17:45 +0200

    Sorry for it being unuseable but now i fixed all errors

--- src/david/practice/DavidsProgram.java
+++ src/david/practice/DavidsProgram.java
@@ -10,31 +10,36 @@ class DavidsProgram{
 	
 System.out.println("Hello welcome to Davids calculator.We dont have alott yet because  this is the beta 0.0.0.0.1. and sorry for gliches this program is being made by a 9 year old. ");
 System.out.print("press 1 to go to the calculator press 2 to see the realise scedule");
-int A;
+int A = Integer.valueOf(new BufferedReader(new InputStreamReader(System.in)).readLine());;
 if(A==2) {System.out.println("Welcome to the release scedule.  ");
 System.out.println("0.0.0.0.1, 2019.july.26");
 System.out.println("0.0.0.0.2, 2019.july.28");
-System.out.println("0.0.0.0.3, 2019.july.31");};
-if(A==1){System.out.println("Welcome to the calculator. For now you can only +,-,/ and * but if you want to know when you can calculate more complex eqasions chek the realise schedule");
+System.out.println("0.0.0.0.3, 2019.july.29");
+System.out.println("0.0.0.0.4, 2019.july.31");};
+if(A==1){System.out.println("Welcome to the calcsttulator. For now you can only +,-,/ and * but if you want to know when you can calculate more complex eqasions chek the realise schedule");
   System.out.println("Press +,-,*,/ depending on wich eqaison you want to do");
  String B = new BufferedReader(new InputStreamReader(System.in)).readLine();
    if(B.equals("-")) {System.out.println("Now pick two numbers and then the computer will do the rest. ");
-   int E;
-   int F;
+   int E = Integer.valueOf(new BufferedReader(new InputStreamReader(System.in)).readLine());
+   int F = Integer.valueOf(new BufferedReader(new InputStreamReader(System.in)).readLine());;
    System.out.println(E-F);
    };
    if(B.equals("+")) {System.out.println("Now pick two numbers and then the computer will do the rest. ");
-   int I;
-   int G;
-   System.out.println(I+G);};
+   int J = Integer.valueOf(new BufferedReader(new InputStreamReader(System.in)).readLine());;
+   int G = Integer.valueOf(new BufferedReader(new InputStreamReader(System.in)).readLine());;
+   System.out.println(J+G);};
    
    if(B.equals("*")) {System.out.println("Now pick two numbers and then the computer will do the rest. ");
-   int G;
-   int H;
-   System.out.println(G*H);
+   int H = Integer.valueOf(new BufferedReader(new InputStreamReader(System.in)).readLine());;
+   int I = Integer.valueOf(new BufferedReader(new InputStreamReader(System.in)).readLine());;
+   System.out.println(I*H);
    };
    if(B.equals("/")) {System.out.println("Now pick two numbers and then the computer will do the rest. ");
+   int L = Integer.valueOf(new BufferedReader(new InputStreamReader(System.in)).readLine());;
+   int M = Integer.valueOf(new BufferedReader(new InputStreamReader(System.in)).readLine());;
+   System.out.println(L/M);
    };
+   
   }
 }
 }
